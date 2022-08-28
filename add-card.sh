a389f0abd630c9f4fe672f197b3325e795265101
laert.gjoni <laert.gjoni@lynxspa.com>
2022-08-28 22:04:48 +0200

    changed implementation, better visuals

---
+++ add-card.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#"USAGE -> ./add-card -a card1 -b card2 -c card3 -d card4 -e card5 -f fusion"
+
+while getopts a:b:c:d:e:f: flag
+do
+	case "${flag}" in
+		a) card1=${OPTARG};;
+		b) card2=${OPTARG};;
+		c) card3=${OPTARG};;
+		d) card4=${OPTARG};;
+		e) card5=${OPTARG};;
+		f) fusion=${OPTARG};;
+	esac
+done
+
+RES=$(echo "${card1:+$card1,}${card2:+$card2,}${card3:+$card3,}${card4:+$card4,}${card5:+$card5,}=$fusion" | sed -e 's/,=/=/g')
+echo $RES >> fusions.csv
+echo "FUSION ADDEDD SUCCESSFULLY"
